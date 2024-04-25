b59b2a2ea79cdc2e191ce8460055591afd1ff494
Jason Kahn <jjjkahn@gmail.com>
2024-04-24 22:17:30 -0400

    Initial commit of the 3 projects

---
+++ RiverLoggerMobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RiverLoggerMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RiverLoggerMobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RiverLoggerMobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
